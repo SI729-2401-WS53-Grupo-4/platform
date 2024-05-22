f274d8615e47763253f814a0ea70bda9081ba236
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-05-22 17:55:14 -0500

    added folders

---
+++ taste-tour-platform/src/main/java/pe/edu/upc/proyect/tastetourplatform/shared/interfaces/rest/resources/AuditableModel.java
@@ -0,0 +1,24 @@
+package pe.edu.upc.proyect.tastetourplatform.shared.interfaces.rest.resources;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Data
+public class AuditableModel {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+}
---
+++ taste-tour-platform/src/main/java/pe/edu/upc/proyect/tastetourplatform/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.proyect.tastetourplatform.shared.interfaces.rest.resources;
+
+public record MessageResource(String message) {
+}
