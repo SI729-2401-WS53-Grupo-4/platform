34dd5da2a11121baa914736ebfe70ba2bdabb344
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-05-23 20:02:24 -0500

    added valueobjects

---
+++ taste-tour-platform/src/main/java/pe/edu/upc/proyect/tastetourplatform/tastetour/domain/model/valueobjects/CreateUserStatus.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.proyect.tastetourplatform.tastetour.domain.model.valueobjects;
+
+public enum CreateUserStatus {
+    REQUESTED,
+    CONFIRMED,
+    REJECTED,
+}
---
+++ taste-tour-platform/src/main/java/pe/edu/upc/proyect/tastetourplatform/tastetour/domain/model/valueobjects/PaymentStatus.java
@@ -0,0 +1,8 @@
+package pe.edu.upc.proyect.tastetourplatform.tastetour.domain.model.valueobjects;
+
+public enum PaymentStatus {
+    REQUESTED,
+    CONFIRMED,
+    REJECTED,
+    CANCELLED,
+}
