7a1472847521699a9cc75ecdb50ce18dc02f5a3f
DanielChirinos615 <89100997+DanielChirinos615@users.noreply.github.com>
2024-06-09 10:05:03 -0500

    Updated develop

--- taste-tour-platform/src/main/java/pe/edu/upc/proyect/tastetourplatform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ taste-tour-platform/src/main/java/pe/edu/upc/proyect/tastetourplatform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -15,8 +15,8 @@ public class OpenApiConfiguration {
     var openApi = new OpenAPI();
     openApi
             .info(new Info()
-                    .title("Tour Platform API")
-                    .description("Tour Platform application REST API documentation.")
+                    .title("TasteTour Platform API")
+                    .description("TasteTour Platform application REST API documentation.")
                     .version("v1.0.0")
                     .license(new License().name("Apache 2.0")
                             .url("https://springdoc.org")))
